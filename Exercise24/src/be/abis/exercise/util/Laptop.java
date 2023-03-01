bf07f0b864583527c717aff04af3f8ab189f93a6
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-03-01 10:56:17 +0100

    generics

--- Exercise24/src/be/abis/exercise/util/Laptop.java
+++ Exercise24/src/be/abis/exercise/util/Laptop.java
@@ -4,19 +4,11 @@ import java.util.List;
 
 public class Laptop {
 
-   /* public static <T> void callPrinter(List<T> list){
+   public static <T> void callPrinter(List<T> list){
       // for (T t : list) new Printer<T>().print(t);
       //OR
         Printer<T> printer = new Printer<>();
         list.forEach(printer::print);
-    }*/
-
-
-    public static void callPrinter(List<? extends Number> list){
-        // for (T t : list) new Printer<T>().print(t);
-        //OR
-       list.forEach(System.out::println);
-       //list.add(new Object());
     }
 
 }
