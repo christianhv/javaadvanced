3ed1af4b61b5c0128368383a2d02d176a8e57297
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-03-01 14:55:04 +0100

    I18N

---
+++ Exercise35/src/be/abis/exercise/repository/CourseRepository.java
@@ -0,0 +1,15 @@
+package be.abis.exercise.repository;
+
+import be.abis.exercise.model.Course;
+
+import java.util.List;
+
+public interface CourseRepository {
+
+    List<Course> findAllCourses();
+    void addCourse(Course c);
+    String formatCourse(Course c);
+    void printAllCourses(String language);
+
+
+}
